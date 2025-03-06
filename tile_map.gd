ed265a1730b11f2e75aca15eae6ec57641761cb0
Mariateb <sabmou05@gmail.com>
2025-03-06 16:15:55 +0100

    Génération procédurale (à refaire pour avoir une île)

--- tile_map.gd
+++ tile_map.gd
@@ -4,12 +4,24 @@ const MAP_SIZE = 215
 const TILE_SIZE = 32
 const TILE_DIRT = Vector2i(0, 0)
 const TILE_WATER = Vector2i(1, 0)
+const TILE_SAND = Vector2i(2, 0)
+var noise = FastNoiseLite.new()
 
 func _ready():
+	noise.noise_type = FastNoiseLite.TYPE_SIMPLEX
+	noise.frequency = 0.01
 	generate_map()
+
+func get_simplex_noise(pos: Vector2):
+	return noise.get_noise_2d(pos.x, pos.y)
 
 func generate_map():
 	for x in range(MAP_SIZE):
 		for y in range(MAP_SIZE):
-			var tile = TILE_DIRT if randf() > 0.3 else TILE_WATER
+			var noise_value = get_simplex_noise(Vector2(x, y))
+			var tile = TILE_WATER
+			if noise_value > -0.4:
+				tile = TILE_SAND
+			if noise_value > -0.2:
+				tile = TILE_DIRT
 			set_cell(0, Vector2i(x, y), 1, tile)
