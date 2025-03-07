79b13088cc211cdc5602b4820a7ae80a7d50d0e6
Bilal EMOHMADIAN <bilal@iscarioth.org>
2025-03-07 06:48:56 +0100

    La totale

--- data_structure/weapon.gd
+++ data_structure/weapon.gd
@@ -14,12 +14,13 @@ func _init(p: Player, wn) -> void:
 	self.player = p
 	self.weapon_name = wn
 	global_position = p.global_position
-	z_index = 5
+	z_index = 1
 	init_collision()
 	init_animation()
 
 func init_collision():
 	area = Area2D.new()
+	area.collision_mask = 1<<3;
 	add_child(area)
 
 	var shape = RectangleShape2D.new()
@@ -84,9 +85,12 @@ func update_position() -> void:
 			self.global_rotation_degrees = 0
 
 func attack():
+	await get_tree().process_frame
 	var bodies = area.get_overlapping_bodies()
+	print(bodies)
 	for body in bodies:
+		print(body)
 		if body is Monster:
 			var damage = base_damage[level]
 			print('Attacking monster:', body, ' with damage:', damage)
-			# body.take_damage(damage)
+			body.take_damage(damage)
